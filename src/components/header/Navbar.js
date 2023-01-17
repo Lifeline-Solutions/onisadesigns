4918dd349ad66d16e416421e6c9619aaa550c077
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-01-17 22:54:10 +0300

    Added styling for the navbar

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/images/logo.png';
+import './Navbar.css';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="logo-div">
-      <img src={logo} alt="logo" className="logo" />
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [active, setActive] = useState('nav-menu');
+  const [toggleItem, setToggleItem] = useState('nav-toggler');
+
+  const toggleMenu = () => {
+    if (active === 'nav-menu') {
+      setActive('nav-menu nav-active');
+    } else {
+      setActive('nav-menu');
+    }
+
+    if (toggleItem === 'nav-toggler') {
+      setToggleItem('nav-toggler toggle');
+    } else {
+      setToggleItem('nav-toggler');
+    }
+  };
+  return (
+    <nav className="nav">
+      <div className="logo-div">
+        <img src={logo} alt="logo" className="logo" />
+      </div>
+      <div className="nav-links">
+        <ul className={active}>
+          <li className="nav-item">Kijani</li>
+          <li className="nav-item">Kijani</li>
+          <li className="nav-item">Kijani</li>
+        </ul>
+      </div>
+
+      <div className={toggleItem} onClick={toggleMenu}>
+        <div className="line1" />
+        <div className="line2" />
+        <div className="line3" />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
