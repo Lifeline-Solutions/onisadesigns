db9b14089bc2dff8f3f0f4356e71ecba1fb00c1f
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-01-27 09:15:27 +0300

    Added shield

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Footer.css';
 
-import image from '../../assets/images/shieldnew.png';
+import image from '../../assets/images/Shieldd.png';
 
 const Footer = () => (
   <div>
-    <footer className="footer">
-      <div className="footer__text">
+    <footer className="footer d-flex justify-content-between">
+      <div className="footer__text w-50">
         <p>@2022 Onisa Designs limited. All rights reserved.</p>
       </div>
       <div
-        className="footer__logo d-flex justify-content-end"
+        className="footer__logo d-flex justify-content-end mr-0 pr-0"
         style={{ margin: '0 10px' }}
       >
         <img src={image} alt="stock" />
